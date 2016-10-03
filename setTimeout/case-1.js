35a2371d37730afd9d9909692ece9f82bf20b9cc
Alex Plex <atellmer@gmail.com>
2016-10-03 14:47:42 +0500

    first commit

---
+++ setTimeout/case-1.js
@@ -0,0 +1,15 @@
+//setTimeout курильщика
+for (var i = 0; i < 10; i++) {
+	setTimeout(function() {
+		console.log(i);
+	}, 0);
+}
+//выведет 10 раз по 10
+
+//setTimeout здорового человека
+setTimeout(function() {
+	for (var i = 0; i < 10; i++) {
+		console.log(i);
+	}
+});
+//выведет 10 раз числа от 0 до 9
