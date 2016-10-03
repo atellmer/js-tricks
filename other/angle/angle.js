7908004171702a22a668432926ee32432233d57b
Alex Plex <atellmer@gmail.com>
2016-10-03 15:09:00 +0500

    new example added

---
+++ other/angle/angle.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//угол между стрелками аналоговых часов по времени
+function getAngle(time, separator) {
+	var h = parseInt(time.split(separator)[0]);
+	var m = parseInt(time.split(separator)[1]);
+
+	if (h >= 24 || h < 0 || m >= 60 || m < 0) {
+		return false;
+	}
+
+	if (h >= 12) {
+		h -= 12;
+	}
+
+	return Math.abs(m * 6 - (h * 30 + m * 0.5));
+}
+
+console.log('Угол: ' + getAngle('00:15', ':') + ' градусов');
+
