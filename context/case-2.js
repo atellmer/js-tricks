c79f68172f9f51093ebabc875882b67b679103e6
Alex Plex <atellmer@gmail.com>
2016-10-13 14:13:47 +0500

    new example added

---
+++ context/case-2.js
@@ -0,0 +1,31 @@
+//Полифил для bind ('binder')
+
+if (typeof Function.prototype.binder === 'undefined') {
+	Function.prototype.binder = function() {
+			var method = this;
+			var context = arguments[0];
+			var args = [];
+			var i = 1;
+
+			for (; i < arguments.length; i++) {
+				args[i - 1] = arguments[i];
+			}
+
+			return function() {
+				return method.apply(context, args);
+			}
+	}
+}
+
+var user = {
+	name: 'Alex',
+	say: function(a, b, c) {
+		return 'I am ' + this.name + ' ' + a + ' ' + b + ' ' + c;
+	}
+}
+
+function fn(callback) {
+	return callback();
+}
+
+console.log(fn(user.say.binder(user, 1, 2, 3)));
