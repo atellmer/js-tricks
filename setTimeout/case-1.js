64e69e1c2936fff0bec36498602a1ab43ec89ad1
Alex Plex <atellmer@gmail.com>
2016-10-19 20:02:13 +0500

    example modified

--- setTimeout/case-1.js
+++ setTimeout/case-1.js
@@ -6,10 +6,13 @@ for (var i = 0; i < 10; i++) {
 }
 //выведет 10 раз по 10
 
+
 //setTimeout здорового человека
-setTimeout(function() {
-	for (var i = 0; i < 10; i++) {
-		console.log(i);
-	}
-}, 0);
-//выведет 10 раз числа от 0 до 9
+for (var i = 0; i < 10; i++) {
+	(function (i) {
+		setTimeout(function() {
+			console.log(i);
+		}, 0);
+	})(i);
+}
+//выведет числа от 0 до 9
